5a77db9fb459912b8984246d94a3f9a8b895a494
CSeb96 <cyrilsebastian96@gmail.com>
2018-05-07 21:22:04 +1200

    Reverting back

--- server.py
+++ server.py
@@ -27,11 +27,11 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = is_msg(message)
+                    response_sent_text = get_message()
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    response_sent_nontext = is_msg(message)
+                    response_sent_nontext = get_message()
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
