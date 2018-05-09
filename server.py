51288a48bff1aa0e8cce4e7bb89a4b956aeb5718
CSeb96 <cyrilsebastian96@gmail.com>
2018-05-09 22:04:06 +1200

    adding listoflinks.count

--- server.py
+++ server.py
@@ -67,7 +67,7 @@ def reply(msg):
     for links in list_links:
          listoflinks = links
 
-    return 'Crawl Finished'
+    return listoflinks.count
         #request = ai.text_request()
         #request.query = msg
 
